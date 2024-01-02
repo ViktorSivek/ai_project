b5de39c447aebf2559ce232050d0542e20a67fa6
Viktor Sívek <sivv01@vse.cz>
2024-01-02 20:07:09 +0100

    templates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-import Image from 'next/image'
+import Link from 'next/link';
 
-export default function Home() {
+export default function Page() {
   return (
-    <div>Ahoj more</div>
-  )
+    <div className="flex items-center justify-center h-screen bg-gray-50">
+      <div className="relative  space-y-4 p-8 border-solid border-2 rounded-lg border-indigo-600">
+        <p className=" text-center text-gray-800 text-3xl">
+          <strong>Zdarek parek</strong>
+        </p>
+        <p className=" text-gray-800 text-3xl">
+          Zkus demo chatbota
+        </p>
+        <Link
+          href="/chat"
+          className="flex justify-center items-center gap-5 self-center rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+        >
+          <span>Lets go</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
