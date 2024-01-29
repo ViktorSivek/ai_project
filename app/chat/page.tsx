7f19162bedd730a82794dcd78982bbaf452570d2
Viktor Sívek <sivv01@vse.cz>
2024-01-29 18:02:26 +0100

    json data in chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -4,13 +4,20 @@ import Navbar from "@/components/navbar";
 import Chat_window from "@/components/chat_window";
 import Table from "@/components/table";
 import Footer from "@/components/footer";
+import React, { useState } from 'react';
 
 export default function Chat() {
+  const [chatData, setChatData] = useState<string>('');
+
+  const handleDataFetch = (jsonString: string) => {
+    setChatData(jsonString);
+  };
+
   return (
     <>
       <Navbar />
-      <Table />
-      <Chat_window />
+      <Table onDataFetch={handleDataFetch} />
+      <Chat_window jsonData={chatData} />
       <Footer />
     </>
   );
